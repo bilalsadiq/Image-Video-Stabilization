9772aaf923e350d0a5600b7b112e2780be783c38
jasonm718 <jasonm718@gmail.com>
2017-04-10 10:18:25 -0500

    Update FaceDetection.py
    Now also detects bodies

--- src/FaceDetection.py
+++ src/FaceDetection.py
@@ -2,18 +2,36 @@ import numpy as np
 import cv2
 
 
+def inside(r, q):
+    rx, ry, rw, rh = r
+    qx, qy, qw, qh = q
+    return rx > qx and ry > qy and rx + rw < qx + qw and ry + rh < qy + qh
+
+
+def draw_detections(img, rects, thickness = 1):
+    for x, y, w, h in rects:
+        pad_w, pad_h = int(0.15*w), int(0.05*h)
+        cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 0, 255), thickness)
+
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-cap = cv2.VideoCapture(0)
+#Prereq for Detection of People
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector( cv2.HOGDescriptor_getDefaultPeopleDetector() )
 
+cap = cv2.VideoCapture(0)
 
 while 1:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    #Detects People
+    found,w=hog.detectMultiScale(img, winStride=(8,8), padding=(32,32), scale=1.5)
+    draw_detections(img,found)
+
     #Detects face
-    
     faces = face_cascade.detectMultiScale(
         gray,
         scaleFactor = 1.1,
@@ -30,6 +48,8 @@ while 1:
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    
 
     #Prints image
     cv2.imshow('Image',img)
